9bb2fd7351c95bbf5f843fc1adf0750c3a0e2943
Federico <crudofederico@gmail.com>
2020-12-11 16:04:12 +0100

    update

---
+++ bonus/js/script.js
@@ -0,0 +1,7 @@
+
+
+// var nome=prompt("Inserisci il tuo nome: ");
+// var cognome=prompt("Inserisci il tuo cognome: ");
+// var colore=prompt("Inserisci il tuo colore: ");
+
+// document.getElementById('password').innerHTML= 'La tua passowrd è: '+nome+cognome+colore+19 ;
