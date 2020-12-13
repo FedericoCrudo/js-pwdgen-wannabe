1394051ecdfcd4d0ca6c6e684f93ed4885b98c5e
Federico <crudofederico@gmail.com>
2020-12-13 15:36:39 +0100

    bonus complete

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,7 +1,41 @@
 
+function copy(){
+    var contenuto=document.getElementById('result-password');
+    //selezioniamo il contenuto
+    contenuto.select();
+    document.execCommand("Copy");
+}
 
-// var nome=prompt("Inserisci il tuo nome: ");
-// var cognome=prompt("Inserisci il tuo cognome: ");
-// var colore=prompt("Inserisci il tuo colore: ");
+function password(){
+    var nome=document.getElementById('nome');
+    var cognome=document.getElementById('cogn');
+    var colore=document.getElementById('color');
+   
+    //controllo campi
+    if(nome.value==""|| cognome.value==""|| colore.value==""){
+        document.getElementById('message').innerHTML= '<span id="text"><span></i>';
+        document.getElementById('text').innerHTML= '* Si prega di compilare tutti i campi';
+        document.getElementById('result').innerHTML= '' ;
+    }
+    else{
+        document.getElementById('message').innerHTML= '';
+       
+       document.getElementById('result').innerHTML=
+        '<span>La tua passowrd è:'+'<textarea id="result-password">'+ nome.value+cognome.value +colore.value+19 + '</textarea>'+ '<i onclick="copy()" class="far fa-clipboard">'+'</span>' ;
+       
+       
+       
+        // if(isNaN(nome)){//per controllare che i valori inseriti non siano numerici
+        //     document.getElementById('text').innerHTML= '* ';   
+        //     console.log(isNaN(nome));
+        // }
+        // else{
+        //     document.getElementById('text').innerHTML= '* ';  
+        
+        // }
+      
+          
+    }
 
-// document.getElementById('password').innerHTML= 'La tua passowrd è: '+nome+cognome+colore+19 ;
+    
+}
